878b2a0248f43dd75a26ec8cc265010e88fb21be
Ayaz Siddiqui <asiddiq2@ncsu.edu>
2021-08-03 20:32:37 -0400

    added project startup file. tested calcNormalizedPathCoords_ts.m. works fine.

--- testScripts/functionTestScripts/calcNormalizedPathCoords_ts.m
+++ testScripts/functionTestScripts/calcNormalizedPathCoords_ts.m
@@ -1,11 +1,13 @@
 clear
 clc
+close all;
 
 %% test calcNormalizedPathCoords.m
 pathWidth   = 40;
 pathHeight  = 12;
 pathElev    = 30;
-pathParam   = linspace(0,0.25,101)*2*pi;
+pathParam   = linspace(0,1,501)*2*pi;
 
-
+plotDome(1);
 rPath = calcNormalizedPathCoords(pathWidth,pathHeight,pathElev,pathParam);
+plot3(rPath(1,:),rPath(2,:),rPath(3,:),'k-');
