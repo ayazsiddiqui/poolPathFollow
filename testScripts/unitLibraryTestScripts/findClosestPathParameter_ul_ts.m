f41fe2fa0ff61cb60708f6277ada4ec15432d079
Ayaz Siddiqui <asiddiq2@ncsu.edu>
2021-08-04 12:42:16 -0400

    created test harness for findClosestPathParameter_ul.slx and verified that the unit library works

--- testScripts/unitLibraryTestScripts/findClosestPathParameter_ul_ts.m
+++ testScripts/unitLibraryTestScripts/findClosestPathParameter_ul_ts.m
@@ -4,3 +4,10 @@ close all
 
 %% test findClosestPathParameter_ul.slx
 findClosestPathParameter_ts
+
+open('findClosestPathParameter_th');
+sim('findClosestPathParameter_th');
+
+% delete temporary simulink files
+rmdir('slprj', 's')
+delete *.slxc
